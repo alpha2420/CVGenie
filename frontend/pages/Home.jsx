8f7240deaf065a94a8f813371504cdcfc18f8c5f
HirdyanshKumar <kumarhirdyansh25@gmail.com>
2026-02-17 18:11:04 +0530

    implement Dashboard,login, Home pages

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
+import Testimonial from '../components/home/Testimonial'
+import Footer from '../components/home/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <Features />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
